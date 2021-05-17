cc3720448c5e5c6e33148e18e45b3c391c9f9176
Khadra Mohamed <khadra.m123@gmail.com>
2021-05-17 18:02:51 -0400

    questions with inquirer

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
+
+/* const fs = require('fs');
+const generatePage = require('./src/page-template');
+
 
 //calls upon the src page-template.js file for the generate page function
 const generatePage = require("./src/page-template.js");
 
-const profileDataArgs = process.argv.slice(2);
-
-const [name, github] = profileDataArgs;
-
+const pageHTML = generatePage(name, github);
 
 
 fs.writeFile('./index.html', generatePage(name, github), err => {
@@ -14,3 +15,16 @@ fs.writeFile('./index.html', generatePage(name, github), err => {
 
   console.log('Portfolio complete! Check out index.html to see the output!');
 });
+ */
+
+inquirer
+  //also known as the question object; identify the name, type and question message 
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  //the answer object is returned as a promise
+  .then(answers => console.log(answers));
